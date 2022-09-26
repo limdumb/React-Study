8416d87cb3040a2c10893ecbfe144be4e37c44eb
limdumb <dlaruddls@nate.com>
2022-09-26 22:37:41 +0900

    제목 추가

--- src/MainPage/Home.jsx
+++ src/MainPage/Home.jsx
@@ -1,12 +1,21 @@
 // import ContainedButton from 'src/Components/button.jsx'
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import ClickButton from '../Components/button'
+
+const MyDiv = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
 
 export default function main() {
   
   return(
-    <div>
+    <MyDiv>
+      <h1>First React Study Assignment</h1>
       <ClickButton>안녕</ClickButton>
-    </div>
+    </MyDiv>
   )
 }
