bac99a65bc5f8b20831fae7549f06f4b98b310db
limdumb <dlaruddls@nate.com>
2022-09-26 22:11:26 +0900

    Disabled Button 구현

--- src/Components/button.jsx
+++ src/Components/button.jsx
@@ -1,33 +1,51 @@
 import styled from "styled-components"
 import React, { useState } from "react"
+import { isDisabled } from "@testing-library/user-event/dist/utils"
 
 const ConsoleButton = styled.button `
   display: flex;
   justify-content: center;
+  align-items: center;
   border-radius: 3px;
   background-color: #5090D3;
   border: 1px solid black;
+  width: 90px;
+  height: 70px;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  font-weight: bolder;
 `
 
 const DisableButton = styled.button `
   display: flex;
   justify-content: center;
+  align-items: center;
   border-radius: 3px;
-  background-color: #5090D3;
-  border: 1px solid black;
+  border: none;
+  width: 90px;
+  height: 70px;
+  border-radius: 10px;
+  margin-left: 10px;
+  font-size: 15px;
+  font-weight: bolder;
 `
+
 
 const StyleDiv = styled.div `
   display: flex;
   justify-content: center;
   align-items: center;
+
 `
 
 export default function ClickButton() {
-  
+  const [activate, setActivate] =React.useState(false)
+  console.log(activate)
+
   return<StyleDiv>
-        <ConsoleButton onClick={console.log('그코잠 화이팅')}>ConsoleLog</ConsoleButton>
-        <DisableButton>Disable</DisableButton>
+    <DisableButton disabled={activate} onClick={() => setActivate(true)}>Disabled</DisableButton>
     </StyleDiv>
+
   
 }
