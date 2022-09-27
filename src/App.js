06382c9bd265e2954e92368b4e3822390ab22fa0
최준호 <mukdol1127@naver.com>
2022-09-27 15:05:28 +0900

    'Home.jsx연결'

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./MainPage/Home.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <button
-        className="btn"
-        onClick={() => {
-          console.log("그코잠 화이팅!");
-        }}
-      >
-        버튼이닷!
-      </button>
-
-      <button
-        disabled=""
-        onClick={(e) => {
-          e.preventDefault();
-          e.currentTarget.disabled = true;
-        }}
-      >
-        없애버렷!
-      </button>
-    </div>
-  );
+  return <Home></Home>;
 }
 
 export default App;
