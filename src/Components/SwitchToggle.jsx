7a1c31acf75f70e3bb7601fa4795f919d875b832
limdumb <dlaruddls@nate.com>
2022-09-29 11:14:02 +0900

    SwitchToggle 컴포넌트 생성

---
+++ src/Components/SwitchToggle.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const AlertSwitch = styled.input`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+  vertical-align:middle;
+`
+
+export default function SwitchToggle() {
+  
+}
