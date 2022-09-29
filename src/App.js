0ccb050f6a681ac2a2955b6e08b48d146d221920
limdumb <dlaruddls@nate.com>
2022-09-29 11:13:34 +0900

    Route 진행

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import Home from './MainPage/Home'
+import Assignment from './MainPage/Assignment'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return(
-    <Home></Home>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/assignment' element={<Assignment/>}/>
+
+        </Routes>
+    </BrowserRouter>
   )
 }
 
