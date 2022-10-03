c3b1c2a266318404b85e56bdfa1d17f4fe29b498
최준호 <mukdol1127@naver.com>
2022-10-03 20:01:10 +0900

    feat: switch로 다크모드 구현

--- src/MainPage/switch.jsx
+++ src/MainPage/switch.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import "../css/switch.css";
 export default function Switch() {
+  // return (
+  //   <div>
+  //     <button>라우터다!!!</button>
+  //   </div>
+  // );
   return (
-    <div>
-      <button>라우터다!!!</button>
+    <div className="App">
+      <DarkMode />
+    </div>
+  );
+}
+
+function DarkMode() {
+  // 👇 다크 모드 On/Off를 상태로 관리!
+  const [isDarkModeOn, setDarkMode] = useState(false);
+  return (
+    <div className={isDarkModeOn ? "Container dark" : "Container light"}>
+      <div className={isDarkModeOn ? "dark" : "light"}>다크다크다크!</div>
+      <label className="switch">
+        <input
+          type="checkbox"
+          onChange={() => {
+            setDarkMode(isDarkModeOn ? false : true);
+          }}
+        />
+        <span className="slider circle"></span>
+      </label>
     </div>
   );
 }
