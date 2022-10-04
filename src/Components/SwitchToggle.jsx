baf446eee540c91eab94339c8ef0d5e43cf9ca61
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-10-04 13:43:55 +0900

    홈 버튼 생성

--- src/Components/SwitchToggle.jsx
+++ src/Components/SwitchToggle.jsx
@@ -1,17 +1,22 @@
+import React from 'react';
 import './SwitchToggle.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 export default function SwitchToggle() {
     return (
         <>
+        <FontAwesomeIcon icon={faHouse} size="5x"/>
         <h1>제껴보세요</h1>
         <label className="switch">
-            <input type="checkbox"></input>
+            <input type="checkbox" onClick={TogleAlert}></input>
             <span className="slider round"></span>
         </label>
         </>     
     )
 }
 
+//함수 선언식으로 호이스팅 적용
 function TogleAlert() {
     setTimeout(() => {
         alert('그 코딩실력에 잠이와? TIL은 썼니?')
