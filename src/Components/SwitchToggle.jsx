91e1f8bd85f1ec7f2c0e9da4bf67017527392d52
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-10-04 13:11:36 +0900

    토글 스위치 css 수정

--- src/Components/SwitchToggle.jsx
+++ src/Components/SwitchToggle.jsx
@@ -2,10 +2,13 @@ import './SwitchToggle.css';
 
 export default function SwitchToggle() {
     return (
+        <>
+        <h1>제껴보세요</h1>
         <label className="switch">
             <input type="checkbox"></input>
             <span className="slider round"></span>
-        </label>     
+        </label>
+        </>     
     )
 }
 
