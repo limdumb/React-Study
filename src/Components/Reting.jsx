ec431a717c79622a48ed8c3dd223a4ae150dcc06
limdumb <dlaruddls@nate.com>
2022-10-06 13:59:59 +0900

    Reting 구현 방향성 변경

--- src/Components/Reting.jsx
+++ src/Components/Reting.jsx
@@ -1,35 +1,16 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-const EmptyStar = styled.button `
-  font-size: 54px;
-  cursor: pointer;
-  background-color: white;
-  border: none;
-
-`
-const BlackStar = styled.button `
-  font-size: 54px;
-  cursor: pointer;
-  border: none;
-  background-color: white;
-`
-//
+//Span으로 구현하기에는 무리가 있다고 판단..
+//image를 노출시켜서 해당 이미지들에 onclick을 주어서 이벤트 설정
+//useState로 해당 image 별로 count 값을 줄것
+//setCount로 해당 count 값 핸들링하여 클릭했을때 그값만큼 검정별이 밑에 노출되게설정
 export default function Reting() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <BlackStar onClick={() => console.log(1)}>★</BlackStar>
-      <BlackStar onClick={() => console.log(2)}>★</BlackStar>
-      <BlackStar onClick={() => console.log(3)}>★</BlackStar>
-      <BlackStar onClick={() => console.log(4)}>★</BlackStar>
-      <BlackStar onClick={() => console.log(5)}>★</BlackStar>
-      <EmptyStar onClick={() => console.log(1)}>☆</EmptyStar>
-      <EmptyStar onClick={() => console.log(2)}>☆</EmptyStar>
-      <EmptyStar onClick={() => console.log(3)}>☆</EmptyStar>
-      <EmptyStar onClick={() => console.log(4)}>☆</EmptyStar>
-      <EmptyStar onClick={() => console.log(5)}>☆</EmptyStar>
+
     </>
   );
 }
