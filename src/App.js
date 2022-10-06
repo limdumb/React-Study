5e38123f6bdae61f2527b35c216ed8ca1772255a
Jangs <wkdwnsdlr37@gmail.com>
2022-10-06 13:34:47 +0900

    [Docs] 영역컴포넌트생성

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./component/Header"
+import Main from "./component/Main"
+import Footer from "./component/Footer"
+
+
+function Add(){
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default Add;
