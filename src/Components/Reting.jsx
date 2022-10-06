e1db8c9f2e01c0580eb2bdcbb15a83ad8a10e40b
limdumb <dlaruddls@nate.com>
2022-10-06 13:50:27 +0900

    전/후 별들 노출

--- src/Components/Reting.jsx
+++ src/Components/Reting.jsx
@@ -1,8 +1,25 @@
+import styled from "styled-components";
 
-
+const EmptyStar = styled.span `
+  font-size: 54px;
+`
+const BlackStar = styled.span `
+  font-size: 54px;
+`
 export default function Reting() {
 
   return (
-    <button>Hi</button>
+    <>
+      <BlackStar>★</BlackStar>
+      <BlackStar>★</BlackStar>
+      <BlackStar>★</BlackStar>
+      <BlackStar>★</BlackStar>
+      <BlackStar>★</BlackStar>
+      <EmptyStar>☆</EmptyStar>
+      <EmptyStar>☆</EmptyStar>
+      <EmptyStar>☆</EmptyStar>
+      <EmptyStar>☆</EmptyStar>
+      <EmptyStar>☆</EmptyStar>
+    </>
   );
 }
