fe78593090a8762a09b4c2899798b67ecd40ca54
limdumb <dlaruddls@nate.com>
2022-10-06 13:58:03 +0900

    Onclick 이벤트 적용

--- src/Components/Reting.jsx
+++ src/Components/Reting.jsx
@@ -1,25 +1,35 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const EmptyStar = styled.span `
+const EmptyStar = styled.button `
   font-size: 54px;
+  cursor: pointer;
+  background-color: white;
+  border: none;
+
 `
-const BlackStar = styled.span `
+const BlackStar = styled.button `
   font-size: 54px;
+  cursor: pointer;
+  border: none;
+  background-color: white;
 `
+//
 export default function Reting() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <BlackStar>★</BlackStar>
-      <BlackStar>★</BlackStar>
-      <BlackStar>★</BlackStar>
-      <BlackStar>★</BlackStar>
-      <BlackStar>★</BlackStar>
-      <EmptyStar>☆</EmptyStar>
-      <EmptyStar>☆</EmptyStar>
-      <EmptyStar>☆</EmptyStar>
-      <EmptyStar>☆</EmptyStar>
-      <EmptyStar>☆</EmptyStar>
+      <BlackStar onClick={() => console.log(1)}>★</BlackStar>
+      <BlackStar onClick={() => console.log(2)}>★</BlackStar>
+      <BlackStar onClick={() => console.log(3)}>★</BlackStar>
+      <BlackStar onClick={() => console.log(4)}>★</BlackStar>
+      <BlackStar onClick={() => console.log(5)}>★</BlackStar>
+      <EmptyStar onClick={() => console.log(1)}>☆</EmptyStar>
+      <EmptyStar onClick={() => console.log(2)}>☆</EmptyStar>
+      <EmptyStar onClick={() => console.log(3)}>☆</EmptyStar>
+      <EmptyStar onClick={() => console.log(4)}>☆</EmptyStar>
+      <EmptyStar onClick={() => console.log(5)}>☆</EmptyStar>
     </>
   );
 }
