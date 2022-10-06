d95f7fcca7ca664a4f399a43ecc908781d4800b7
limdumb <dlaruddls@nate.com>
2022-10-06 13:16:08 +0900

    [Refactor] 불필요한 코드 삭제 및 수정

--- src/Components/button.jsx
+++ src/Components/button.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import React, { useState } from "react"
 import { isDisabled } from "@testing-library/user-event/dist/utils"
 
-const ConsoleButton = styled.button `
+const StyledButton = styled.button `
   display: flex;
   justify-content: center;
   align-items: center;
@@ -15,6 +15,7 @@ const ConsoleButton = styled.button `
   border-radius: 10px;
   color: white;
   font-weight: bold;
+  margin-left: 10px;
   &:hover {
     background-color: #649ad5;
   color: black;
@@ -53,57 +54,6 @@ const DisableButton = styled.button `
   cursor: pointer;
   }
 `
-const OnOffButton = styled.button `
-  margin-left: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 3px;
-  background-color: #5090D3;
-  border: 1px solid black;
-  width: 90px;
-  height: 70px;
-  border: none;
-  border-radius: 10px;
-  color: white;
-  font-weight: bold;
-  &:hover {
-    background-color: #649ad5;
-  color: black;
-  cursor: pointer;
-  }
-  &:active {
-  box-shadow: 1px 1px 0 rgb(0,0,0,0.5);
-  position: relative;
-  top:2px;
-}
-`
-
-const AssignmentButton = styled.button `
-  margin-left: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 3px;
-  background-color: #5090D3;
-  border: 1px solid black;
-  width: 90px;
-  height: 70px;
-  border: none;
-  border-radius: 10px;
-  color: white;
-  font-weight: bold;
-  &:hover {
-    background-color: #649ad5;
-  color: black;
-  cursor: pointer;
-  }
-  &:active {
-  box-shadow: 1px 1px 0 rgb(0,0,0,0.5);
-  position: relative;
-  top:2px;
-}
-`
 
 const StyleDiv = styled.div `
   display: flex;
@@ -124,11 +74,11 @@ export default function ClickButton() {
   }
 
   return<StyleDiv>
-    <ConsoleButton onClick={() => console.log('그코잠 화이팅!')}>Console</ConsoleButton>
-    <OnOffButton onClick={() => onOffToggle(setActivate)}>On/Off</OnOffButton>
+    <StyledButton onClick={() => console.log('그코잠 화이팅!')}>Console</StyledButton>
+    <StyledButton onClick={() => onOffToggle(setActivate)}>On/Off</StyledButton>
     <DisableButton disabled={activate} onClick={() => setActivate(true)}>Disabled</DisableButton>
     <a href="/assignment" style={{ textDecoration: 'none' }}>
-      <AssignmentButton>Assignment Move</AssignmentButton>
+      <StyledButton>Assignment Move</StyledButton>
     </a>
     </StyleDiv>
   
