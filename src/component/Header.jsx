84324aece29e39081149453ee47126c07d947b49
Jangs <wkdwnsdlr37@gmail.com>
2022-10-06 16:02:29 +0900

    [Design] Header영역 css적용

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,20 @@
 function Header(){
     return(
         <div className="Header ibx">
-            <h1>상단영역</h1>
+            <h1>React Study</h1>
+            <nav className="GNB">
+                <ul className="GNB_dot_ul">
+                    <li>Home</li>
+                    <li>
+                        <span>과제</span>
+                        <div className="GNB_sub">
+                            <ol className="GNB_sub_ol">
+                                <li>버튼과제</li>
+                            </ol>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
