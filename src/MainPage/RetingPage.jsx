43f4285002592bb91c0c31490ca2c770fb30e803
limdumb <dlaruddls@nate.com>
2022-10-10 18:49:23 +0900

    배경 css 변경

--- src/MainPage/RetingPage.jsx
+++ src/MainPage/RetingPage.jsx
@@ -3,6 +3,15 @@ import Reting from "../Components/Reting";
 export default function RetingPage() {
 
   return (
-    <Reting></Reting>
+    <div style={{
+      display:"flex",
+      flexDirection:"column",
+      alignItems:"center",
+      height:"100vh",
+      backgroundImage:"url(https://blog.kakaocdn.net/dn/c6pAkf/btrn4rgw5la/7m06GPUMq1155ou7EEYo21/img.gif)"
+    }}>
+      <h1 >Reting Star Components</h1>
+      <Reting></Reting>
+    </div>
   );
 }
