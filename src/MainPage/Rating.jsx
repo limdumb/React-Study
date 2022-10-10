167b086b22ae5313a60d59d20215bb7a3ee2b385
최준호 <mukdol1127@naver.com>
2022-10-10 22:57:48 +0900

    RatingStar hover기능 구현

--- src/MainPage/Rating.jsx
+++ src/MainPage/Rating.jsx
@@ -6,15 +6,42 @@ const Array = [0, 1, 2, 3, 4];
 
 function Rating() {
   return (
-    <>
-      <>
+    <Wrap>
+      <Stars>
         {Array.map((el, idx) => {
-          return <FaStar />;
+          return <FaStar key={idx} size="50" />;
         })}
-      </>
-    </>
-    //
+      </Stars>
+    </Wrap>
   );
 }
 
 export default Rating;
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 15px;
+`;
+
+const Stars = styled.div`
+  display: flex;
+  padding-top: 5px;
+
+  & svg {
+    color: black;
+    cursor: pointer;
+  }
+
+  :hover svg {
+    color: gold;
+  }
+
+  & svg:hover ~ svg {
+    color: black;
+  }
+
+  .yellowStar {
+    color: gold;
+  }
+`;
