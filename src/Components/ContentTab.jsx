4e93916a3f96dab07295961f83548abfa4cb1aec
limdumb <dlaruddls@nate.com>
2022-10-13 19:57:40 +0900

    ContentTab Component 추가

---
+++ src/Components/ContentTab.jsx
@@ -0,0 +1,12 @@
+
+
+// 1. state가 아닌 변수를 사용하여 fill메서드와 map메서드를 사용하여 Array만들기
+// 2. 해당 Array를 JSX문법에 map메서드를 사용하여 반복렌더링하기
+// 3. input 태그를 이용하여 value값으로 Array의 값을 하나씩 넣어주기
+
+export default function ContentTab() {
+  
+  return(
+    <button>윈도우 개느림ㅋ</button>
+  )
+}
