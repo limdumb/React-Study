33bcc886b736a8d422a3cd6b3fd8ddf900450de2
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-10-17 03:07:50 +0900

    가득별,빈별 이미지를 불러와줌

--- src/Components/TabButtons.jsx
+++ src/Components/TabButtons.jsx
@@ -1,11 +1,13 @@
-// 1. Tab 버튼을 3개를 생성한다.
+import FullStars from "https://cdn-icons-png.flaticon.com/512/1012/1012350.png"
+import EmptyStars from "https://cdn-icons-png.flaticon.com/512/1012/1012299.png"
+// 1. Tab 버튼을 3개를 생성한다. V
 // 2. Tab1 Onclick시 Rating 기능과 기능에 대한 설명이 밑에 나타나도록 구현
 // 3. Tab2 Onclick시 Modal Component가 나오게끔 구현한다. (컴포넌트 분리해야함)
 // 4. Tab3 Onclick시 Switch Component 구현 (Alert 내용은 스터디 1기가 끝난 소감)
 
 
 
-export default TabButtons = () => {
+const TabButtons = () => {
     return (
     <div>
         <button>TAB1</button>
@@ -14,3 +16,7 @@ export default TabButtons = () => {
     </div>
     )
 }
+// 2. Tab1 Onclick시 Rating 기능과 기능에 대한 설명이 밑에 나타나도록 구현
+
+
+export default TabButtons;
