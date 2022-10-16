c7bbe1cf02d69d53103a3c6e7c6c97be310fa7eb
Jangs <wkdwnsdlr37@gmail.com>
2022-10-16 22:31:02 +0900

    [Feat] NewRating 컴포넌트 추가

--- src/component/nav_Button/NewRating.jsx
+++ src/component/nav_Button/NewRating.jsx
@@ -1,7 +1,34 @@
+import {useState} from "react"
+import styled from "styled-components";
+
+const Circle = styled.div`
+    width:100px;
+    height: 100px;
+    background-color:blue;
+    display: inline-block;
+    border: 1px solid black;
+    border-radius: 50%;
+`
+
 function NewRating(){
+    const [val, setVal] = useState(Array(5).fill(0).map((value, index)=> index + 1 ))
+    const [save, setSvae] = useState(0)
 
     return(
-        <h1>테스트</h1>
+        <>
+        {val.map((el)=>{
+            return (
+                <Circle
+                key={el}
+                value={el}
+                onClick={()=>{
+                    setSvae(el)
+                }}
+                style={{background: el <= save ? "red" : "blue"}}
+                />
+            )
+        })}
+        </>
     )
 
 }
