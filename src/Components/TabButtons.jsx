6db9449a0282db360dd1e500daed084a33ede9fd
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-10-17 00:00:24 +0900

    컴포넌트 이름명 수정

---
+++ src/Components/TabButtons.jsx
@@ -0,0 +1,16 @@
+// 1. Tab 버튼을 3개를 생성한다.
+// 2. Tab1 Onclick시 Rating 기능과 기능에 대한 설명이 밑에 나타나도록 구현
+// 3. Tab2 Onclick시 Modal Component가 나오게끔 구현한다. (컴포넌트 분리해야함)
+// 4. Tab3 Onclick시 Switch Component 구현 (Alert 내용은 스터디 1기가 끝난 소감)
+
+
+
+export default TabButtons = () => {
+    return (
+    <div>
+        <button>TAB1</button>
+        <button>TAB2</button>
+        <button>TAB3</button>
+    </div>
+    )
+}
