d7e930e23525a881b55566db7a476dfe9580dc5b
Jangs <wkdwnsdlr37@gmail.com>
2022-10-16 20:42:13 +0900

    [Design]  step1 컴포넌트 css변경 및 HTML 추가

--- src/component/nav_Button/Step1.jsx
+++ src/component/nav_Button/Step1.jsx
@@ -9,10 +9,14 @@ const Tapimg =styled.img`
 function Step1(){
 
     return(
-        <div>
+        <div className="step1">
             <h1>제목</h1>
             <Tapimg src={`${process.env.PUBLIC_URL}/photo/test1.jpg`} alt="imgs"/>
             <p>Rating내용</p>
+            <ul className="step1_dot_btns">
+                <li>⬅️왼쪽</li>
+                <li>오른쪽➡️</li>
+            </ul>
         </div>
     )
 }
