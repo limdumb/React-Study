a36cd4d111a8920b1e16f89d19d1d26d5639a465
Jangs <wkdwnsdlr37@gmail.com>
2022-10-16 20:28:53 +0900

    [Design] step1컴포넌트 img추가

--- src/component/nav_Button/Step1.jsx
+++ src/component/nav_Button/Step1.jsx
@@ -1,10 +1,17 @@
+import React from "react";
+import styled from "styled-components";
 
+const Tapimg =styled.img`
+    width: 250px;
+    height: 250px;
+`
 
 function Step1(){
 
     return(
         <div>
             <h1>제목</h1>
+            <Tapimg src={`${process.env.PUBLIC_URL}/photo/test1.jpg`} alt="imgs"/>
             <p>Rating내용</p>
         </div>
     )
