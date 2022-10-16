e914350f11e4c2ce795f5dee5478c7563a276269
Jangs <wkdwnsdlr37@gmail.com>
2022-10-16 21:03:14 +0900

    [Feat] step2 클릭시 팝업기능 구현

--- src/component/nav_Button/Step2.jsx
+++ src/component/nav_Button/Step2.jsx
@@ -1,10 +1,14 @@
-
+import {useState} from "react"
 
 function Step2(){
+    const [newpage, SetNewPage] = useState(false)
+    console.log("newpage:",newpage)
 
     return(
-        <div>
-            <button>클릭!</button>
+        <div className="step2">
+            <button onClick={()=> SetNewPage(!newpage)}>클릭!</button>
+            <div className="step2_dot_new" style={{display : newpage ? "block":"none"}}>
+            </div>
         </div>
     )
 }
