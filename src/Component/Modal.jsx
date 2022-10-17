fa781b9ac731c75405c070e86d783f65b8a6f7a3
최준호 <mukdol1127@naver.com>
2022-10-18 03:31:31 +0900

    [feat, Design] Modal 컴포넌트 구현

--- src/Component/Modal.jsx
+++ src/Component/Modal.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import "../css/Modal.css";
+
 function Modal() {
+  const [modal, SettModal] = useState(false);
+
+  const PopupButton = () => {
+    return SettModal(!modal);
+  };
+
   return (
-    <div>
-      <button>모?달</button>
+    <div className="modal_container">
+      <button className="modal_Button" onClick={PopupButton}>
+        윈도우를 탈출하자!
+      </button>
+      <div className={modal ? "backDiv" : null}>
+        {modal ? (
+          <div className="modal_content">
+            <div>
+              <h1>나도 조금 있으면 맥북 오우너...?</h1>
+            </div>
+            <button className="close__Button" onClick={PopupButton}>
+              맥북
+            </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
+
 export default Modal;
