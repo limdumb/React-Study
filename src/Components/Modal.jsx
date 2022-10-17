16c81a031bebb57d8f6a792acb30595edfcfab9d
limdumb <dlaruddls@nate.com>
2022-10-17 23:40:12 +0900

    Modal Components 구현

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,6 +1,5 @@
+import { useState } from "react";
 
-
-export default function Modal() {
 // overay구현 , 전면노출 팝업 구현
 // dismiss
 //1. 버튼을 만든다 O
@@ -10,9 +9,59 @@ export default function Modal() {
 //5. true,false값을 state로 준다
 //6. Return문에서 3항 연산자로 true일때는 div가 팝업되게 false일때는 값이없게
 
+export default function Modal() {
+  const [showModal, setShowModal] = useState(false);
+
+  const togglePopup = () => {
+    return setShowModal(!showModal);
+  };
+
+  console.log(showModal);
   return (
-    <div>
-      <button>Click Me!</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+      >
+      <button style={{
+        width: 120,
+        height: 120,
+        border: "none",
+        borderRadius: 50,
+        backgroundColor:"hsl(344deg 48% 76%)",
+        color: 'white',
+        fontSize: 30,
+        fontWeight: "bold",
+        marginTop: 40,
+        cursor:"pointer"
+      }} onClick={togglePopup}>Click Me!</button>
+      <div
+        className={showModal ? "backDisplay" : ""}
+      >
+        {showModal ? (
+          <div className="modalInner">
+            <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+            <h1 style={{ color: "white" }}>⭐️ 모두들 고생 하셨습니다 ⭐️ </h1>
+            <h2 style={{ color: "white" }}>🎃 되돌아 가고 싶다면 밑에 유령을 눌러주세요 🎃</h2>
+            </div>
+            <button
+              style={{
+                display: "inline",
+                background: "none",
+                color: "white",
+                fontSize: 30,
+                border: "none",
+                fontWeight: "bold",
+                marginBottom:30,
+                cursor:"pointer"
+              }}
+              onClick={togglePopup}>
+              👻
+            </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
