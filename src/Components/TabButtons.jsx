5391844c94863ad709d995bb11a3ea4b1aa90ab8
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-10-18 01:17:22 +0900

    Tab 타이틀 반환

--- src/Components/TabButtons.jsx
+++ src/Components/TabButtons.jsx
@@ -1,16 +1,28 @@
 import RatingStar from "./RatingStar";
-import styled from "styled-components";
+import Modal from "./Modal";
+import SwitchToggle from "./SwitchToggle";
+// import { useState } from "react";
 // 1. Tab 버튼을 3개를 생성한다. V
 // 2. Tab1 Onclick시 Rating 기능과 기능에 대한 설명이 밑에 나타나도록 구현
 // 3. Tab2 Onclick시 Modal Component가 나오게끔 구현한다. (컴포넌트 분리해야함)
 // 4. Tab3 Onclick시 Switch Component 구현 (Alert 내용은 스터디 1기가 끝난 소감)
 
-const Tab = styled.button`
-    width: 100%;
-    height: 10em;
-`
 
 const TabButtons = () => {
+    // const [tab, setTab] = useState(0);
+    const tabContents = [{
+        title: <button>"Rating"</button>,
+        content: <RatingStar/>
+    },
+
+        {
+        title: <button>"Modal"</button>,
+        content: <Modal />
+    },
+        {
+        title: <button>"Switch"</button>,
+        content: <SwitchToggle />
+    }]
     return (
     <div style={{
         border: "2px solid red",
@@ -19,16 +31,20 @@ const TabButtons = () => {
         <nav style={{
             display: "flex",
             flexDirection: "row",
+            justifyContent: "space-around",
         }}>
-        <Tab>TAB1</Tab>
-        <Tab>TAB2</Tab>
-        <Tab>TAB3</Tab>
+        {tabContents.map((el) => {
+            return el.title;
+        })}
         </nav>
             <div style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
                 border: "2px solid blue",
                 height: "80vh"
-            }}>
-            <RatingStar />
+            }} >
+
             </div>
     </div>
     )
