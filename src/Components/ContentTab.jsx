832e6d203b2d710fce90bc1221ae43f61b63876e
limdumb <dlaruddls@nate.com>
2022-10-18 00:49:17 +0900

    Tab Component 구현

--- src/Components/ContentTab.jsx
+++ src/Components/ContentTab.jsx
@@ -1,10 +1,45 @@
-import Reting from "./Reting"
-import SwitchToggle from "./SwitchToggle"
-import Modal from "./Modal"
+import Reting from "./Reting";
+import SwitchToggle from "./SwitchToggle";
+import Modal from "./Modal";
+import { useState } from "react";
 
 export default function ContentTab() {
-  
-  return(
-    <button>윈도우 개느림ㅋ</button>
-  )
+  const [activateTab, setActivateTab] = useState(0);
+
+  const tabClickHendler = (index) => {
+    setActivateTab(index);
+  };
+
+  const contentTab = [
+    {
+      tabTitle: <button onClick={() => tabClickHendler(0)}>Reting</button>,
+      tabContetn: <Reting/>,
+    },
+    {
+      tabTitle: <button onClick={() => tabClickHendler(1)}>Modal</button>,
+      tabContetn: <Modal/>,
+    },
+    {
+      tabTitle: <button onClick={() => tabClickHendler(2)}>Switch</button>,
+      tabContetn: <SwitchToggle/>,
+    },
+  ];
+
+  return (
+    <div className="Container">
+      <div className="tab__Container">
+        {contentTab.map((section) => {
+          return section.tabTitle
+        })}
+      </div>
+        <div style={{
+          display:"flex",
+          justifyContent:"center",
+          alignItems: "center",
+          flexDirection:"column"
+        }}>
+          {contentTab[activateTab].tabContetn}
+        </div>
+    </div>
+  );
 }
