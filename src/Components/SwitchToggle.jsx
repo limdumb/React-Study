8ee88a4194596454c7419bfff5cbbe36c349d8bb
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-10-17 21:16:38 +0900

    SwitchToggle 컴포넌트 함수  리팩토링

--- src/Components/SwitchToggle.jsx
+++ src/Components/SwitchToggle.jsx
@@ -1,34 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SwitchToggle.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faHouse } from '@fortawesome/free-solid-svg-icons';
+// import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+const SwitchDiv = styled.div`
+    border: 2px solid pink;
+    height: 80px;
+    width: 250px;
+    border-radius: 50px;
+    background-color: pink;
 `  ;
+const SwitchSpan = styled.span`
+    display: flex;
+    margin: 3px;
+    height: 70px;
+    width: 70px;
+    border-radius: 50px;
+    cursor: pointer;
+    background-color: green;
+`
 
 export default function SwitchToggle() {
+    const [toggle, setToggle] = useState(false)
+
+    const Toggle = (() => {
+        if(!toggle) {
+            setTimeout(() => {
+                alert("스터디 하길 참 잘했다!!")
+            },1000)
+        }
+        setToggle(!toggle)
+        
+            
+    })
     return (
         <>
-        <StyledDiv>
-        <Link to="/"  className="house"><FontAwesomeIcon icon={faHouse} size="5x"/></Link>
-        <h1>제껴보세요</h1>
-        <label className="switch"> 
-            <input type="checkbox" onClick={TogleAlert}></input>
-            <span className="slider round"></span>
-        </label>
-        </StyledDiv>
-        </>     
+        <div>
+        <SwitchDiv onClick={Toggle}>
+        <SwitchSpan style={{transition: "0.5s", transform: toggle ? "translateX(170px)" : "translateX(0)" }}>
+        </SwitchSpan>
+        </SwitchDiv>
+        </div>
+        </>
     )
-}
-
-//함수 선언식으로 호이스팅 적용
-function TogleAlert() {
-    setTimeout(() => {
-        alert('그 코딩실력에 잠이와? TIL은 썼니??')
-    }, 3000);
 }
