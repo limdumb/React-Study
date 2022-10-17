16ae22ef1d640d209264db2a10bc90d2c0ffbf73
limdumb <dlaruddls@nate.com>
2022-10-17 13:52:37 +0900

    Reting, Switch Component 가져오기

--- src/Components/ContentTab.jsx
+++ src/Components/ContentTab.jsx
@@ -1,8 +1,6 @@
+import Reting from "./Reting"
+import SwitchToggle from "./SwitchToggle"
 
-
-// 1. state가 아닌 변수를 사용하여 fill메서드와 map메서드를 사용하여 Array만들기
-// 2. 해당 Array를 JSX문법에 map메서드를 사용하여 반복렌더링하기
-// 3. input 태그를 이용하여 value값으로 Array의 값을 하나씩 넣어주기
 
 export default function ContentTab() {
   
