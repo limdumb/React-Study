a6a8c6bae2958d8b1284b5017fa5f9964dcec1c7
limdumb <dlaruddls@nate.com>
2022-10-17 22:27:33 +0900

    Modal Components 의사코드 작성

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,11 +1,18 @@
 
 
 export default function Modal() {
-
+// overay구현 , 전면노출 팝업 구현
+// dismiss
+//1. 버튼을 만든다 O
+//2. Overay를 구현한다[ 확인필요 ] [] button click , position absolute사용
+//3. 팝업되는 창을 구현한다 [Div로 해야하나..]
+//4. 버튼을 클릭할때[onclick] overay와 전면에 노출되는 팝업이 render되게 한다
+//5. true,false값을 state로 준다
+//6. Return문에서 3항 연산자로 true일때는 div가 팝업되게 false일때는 값이없게
 
   return (
-    <>
-      <button>하이</button>
-    </>
-  )
+    <div>
+      <button>Click Me!</button>
+    </div>
+  );
 }
