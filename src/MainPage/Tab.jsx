4518829a925db3b85345654bbfd8cdfcc301db8f
최준호 <mukdol1127@naver.com>
2022-10-17 15:07:14 +0900

    [Docs] Tap Page 생성

--- src/MainPage/Tab.jsx
+++ src/MainPage/Tab.jsx
@@ -1,3 +1,27 @@
-export default function Tab() {
-  return <h1>hello</h1>;
+import Step1 from "./Step1";
+import Step2 from "./Step2";
+import Step3 from "./Step3";
+
+function Tabs() {
+  return (
+    <div className="container">
+      <div className="bloc-tabs"></div>
+
+      <div className="content-tabs">
+        <div>
+          <Step1 />
+        </div>
+
+        <div>
+          <Step2 />
+        </div>
+
+        <div>
+          <Step3 />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Tabs;
