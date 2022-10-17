ef057863700421bf793d7036e09c1401a156aa04
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2022-10-17 22:33:54 +0900

    flex로 레이아웃 잡기

--- src/Components/TabButtons.jsx
+++ src/Components/TabButtons.jsx
@@ -1,27 +1,35 @@
-import FullStars from "https://cdn-icons-png.flaticon.com/512/1012/1012350.png"
-import EmptyStars from "https://cdn-icons-png.flaticon.com/512/1012/1012299.png"
-import styled from "styled-components"
+import RatingStar from "./RatingStar";
+import styled from "styled-components";
 // 1. Tab 버튼을 3개를 생성한다. V
 // 2. Tab1 Onclick시 Rating 기능과 기능에 대한 설명이 밑에 나타나도록 구현
 // 3. Tab2 Onclick시 Modal Component가 나오게끔 구현한다. (컴포넌트 분리해야함)
 // 4. Tab3 Onclick시 Switch Component 구현 (Alert 내용은 스터디 1기가 끝난 소감)
 
-const FullImg = styled.img`
-    width: 100px;
-    height: 100px;
-`
-
-const EmptyImg = styled.img`
-    width: 100px;
-    height: 100px;
+const Tab = styled.button`
+    width: 100%;
+    height: 10em;
 `
 
 const TabButtons = () => {
     return (
-    <div>
-        <button>TAB1</button>
-        <button>TAB2</button>
-        <button>TAB3</button>
+    <div style={{
+        border: "2px solid red",
+        height: "100vh",
+        width: "60vw"}}>
+        <nav style={{
+            display: "flex",
+            flexDirection: "row",
+        }}>
+        <Tab>TAB1</Tab>
+        <Tab>TAB2</Tab>
+        <Tab>TAB3</Tab>
+        </nav>
+            <div style={{
+                border: "2px solid blue",
+                height: "80vh"
+            }}>
+            <RatingStar />
+            </div>
     </div>
     )
 }
