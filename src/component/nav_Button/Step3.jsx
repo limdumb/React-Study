4c26021b36197d78bb272fc67abf241cf9677880
Jangs <wkdwnsdlr37@gmail.com>
2022-10-18 18:17:52 +0900

    [Feat] step3 toggle버튼 구현

--- src/component/nav_Button/Step3.jsx
+++ src/component/nav_Button/Step3.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import {useState} from "react"
 import styled from "styled-components"
 
 const Toggle = styled.div`
@@ -6,22 +6,26 @@ const Toggle = styled.div`
     border-radius: 50%;
     width : 50px;
     height : 50px;
-    transform: translateY(-12.5px);
 `
 const Togglebody = styled.div`
     background-color: blue;
     width : 400px;
     height : 25px;
     border-radius: 20px;
+    
 `
 
 function Step3(){
+    const [click, setClick] = useState(false);
 
     return(
         <div>
             <h1>Toggle 버튼!</h1>
-            <Togglebody><Toggle/></Togglebody>
-            <p>내용설명</p>
+            <Togglebody>
+                <Toggle onClick={()=> setClick(!click)}
+                    className={click ? "event1" : "event2"}/>
+            </Togglebody>
+            <p>클릭에따라 css속성이아닌 클래이름 추가로 구현한 Toggle 버튼</p>
         </div>
     )
 }
