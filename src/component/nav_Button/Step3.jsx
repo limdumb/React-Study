8fd0fe51b7fc77b08d92f9eb84e850670c374dab
Jangs <wkdwnsdlr37@gmail.com>
2022-10-19 17:53:43 +0900

    [Feat] step3에 alert 기능추가

--- src/component/nav_Button/Step3.jsx
+++ src/component/nav_Button/Step3.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {useState} from "react"
 import styled from "styled-components"
 
@@ -18,14 +19,23 @@ const Togglebody = styled.div`
 function Step3(){
     const [click, setClick] = useState(false);
 
+    useEffect(()=>{
+        if(click === true){
+            setTimeout(()=>{
+                alert("모두정말고생많으셨습니다 그코잠에 들어오게된후 리액트에대해서 더많이 알게되었습니다. 앞으로도 많이부족하지만 잘부탁드립니다!");
+            },1000);
+        }
+    },[click]);
+
     return(
         <div>
             <h1>Toggle 버튼!</h1>
             <Togglebody>
                 <Toggle onClick={()=> setClick(!click)}
-                    className={click ? "event1" : "event2"}/>
+                    className={click ? "event2" : "event1"}
+                    />
             </Togglebody>
-            <p>클릭에따라 css속성이아닌 클래이름 추가로 구현한 Toggle 버튼</p>
+            <p>클릭에따라 css속성이아닌 클래스이름을 추가하여 구현한 Toggle 버튼</p>
         </div>
     )
 }
